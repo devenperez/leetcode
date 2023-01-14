043507e55e19cd2b0c3f69a615176c7fe979075b
Deven <63876261+devenperez@users.noreply.github.com>
2023-01-13 19:52:50 -0500

    Time: 47 ms (63.61%), Space: 14.1 MB (56.56%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def permute_with_prefix(nums: List[int], prefix: List[int]):
+            # Base case
+            if len(nums) == 1:
+                return [prefix + nums]
+            
+            permutations = []
+            for i in range(len(nums)):
+                 permutations += permute_with_prefix(nums[:i] + nums[(i + 1):], # Remove the ith element from nums
+                                                     prefix + [nums[i]])
+            return permutations
+        
+        # Initial call
+        return permute_with_prefix(nums, [])
