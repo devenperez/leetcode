8e78f828ef0944992025eed1e14966ab9b18c564
Deven <63876261+devenperez@users.noreply.github.com>
2023-01-17 23:51:13 -0500

    Time: 31 ms (90.82%), Space: 13.7 MB (99.87%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -2,9 +2,27 @@ class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         count = 0
         
+        while len(nums) > 0:
+            oldLength = len(nums)
+            
+            # Removes all instances of nums[0]
+            nums = [ n for n in nums if n != nums[0] ]
+            
+            numOccurances = oldLength - len(nums)
+            
+            if numOccurances > 1:
+                count += int((numOccurances * (numOccurances - 1)) / 2)   # numOccurances choose 2
+                
+        return count
+        
+        """
+        ## Straight-forward solution
+        count = 0
+        
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] == nums[j]:
                     count += 1
         
         return count
+        """
