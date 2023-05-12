2f3dd4cb9f2fefe864b524be4ba40fa4357927ee
Deven <63876261+devenperez@users.noreply.github.com>
2023-05-11 20:30:43 -0400

    Time: 30 ms (42.20%), Space: 14.1 MB (65.22%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        array = []
+
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                array.append("FizzBuzz")
+            elif i % 3 == 0:
+                array.append("Fizz")
+            elif i % 5 == 0:
+                array.append("Buzz")
+            else:
+                array.append(str(i))
+            
+        return array;
