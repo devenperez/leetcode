b3261233954b983cd24cad71237e4cf44f6a54f1
Deven <63876261+devenperez@users.noreply.github.com>
2025-09-09 15:10:38 -0400

    Time: 1 ms (35.22%), Space: 12.5 MB (53.48%) - LeetHub

---
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        count = 0
+        for n in nums:
+            if n < k:
+                count += 1
+
+        return count
