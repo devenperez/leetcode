3399d5f921c265be2560ba593eb79bd2190acffe
Deven <63876261+devenperez@users.noreply.github.com>
2025-11-26 14:24:59 -0500

    Time: 0 ms (100%), Space: 18.6 MB (17.59%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,18 +3,22 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        # Total complexities:
+        # O(mn) time
+        # O(m+n) space
+
         # O(m+n) space
         rowsToZero = set()
         colsToZero = set()
 
-        # Find all zeros
+        # Find all zeros - O(mn)
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if matrix[i][j] == 0:
                     rowsToZero.add(i)
                     colsToZero.add(j)
 
-        # Zero rows and cols as needed
+        # Zero rows and cols as needed - O(mn)
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if i in rowsToZero or j in colsToZero:
